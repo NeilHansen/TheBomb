b82804a5f8a5dc744f4a4de7c3705223d35705e8
NeilHansen <neil.hansen17@gmail.com>
2017-10-25 17:19:04 -0400

    basic jump test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	private Rigidbody2D rb;
+	public float jumpThrust;
+	public float dashThrust;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown("space")) 
+		{
+			rb.AddForce(transform.right * jumpThrust);
+		}
+	}
+
+	void FixedUpdate()
+	{
+//		if (Input.GetKey("space")) 
+//		{
+//			rb.AddForce(transform.right * jumpThrust);
+//		}
+	}
+}

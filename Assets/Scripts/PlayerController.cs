664b84814ef6e6bb4ae2f523867dfdc3b1f7f115
NeilHansen <neil.hansen17@gmail.com>
2017-10-25 21:25:41 -0400

    added legit physics to jump, added double jump prototype

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,28 +6,91 @@ public class PlayerController : MonoBehaviour {
 
 
 	private Rigidbody2D rb;
-	public float jumpThrust;
+	public float jumpHeight;
 	public float dashThrust;
+
+	private float finalVel;
+	private float acceleration;
+	private float jumpThrust; 
+
+	private bool isOnGround = true;
+	private bool canDoubleJump = false;
 
 	// Use this for initialization
 	void Start () {
 		rb = this.GetComponent<Rigidbody2D> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		if (Input.GetKeyDown("space")) 
+
+	//Check if your on the ground
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Floor")
 		{
-			rb.AddForce(transform.right * jumpThrust);
+			Debug.Log ("OnGround");
+			isOnGround = true;
 		}
 	}
 
-	void FixedUpdate()
+	// Update is called once per frame
+	void Update () {
+		CheckJump ();
+		CheckDash ();
+
+	}
+
+	//Check For Jump
+	void CheckJump()
 	{
-//		if (Input.GetKey("space")) 
-//		{
-//			rb.AddForce(transform.right * jumpThrust);
-//		}
+		if (Input.GetKeyDown("space")) 
+		{
+			if (isOnGround)
+			{
+				ApplyJump ();
+				canDoubleJump = true;
+				isOnGround = false;
+			} 
+			else 
+			{
+				if (canDoubleJump) 
+				{
+					canDoubleJump = false;
+					ApplyJump ();
+				}
+			}
+		}
+	}
+
+	//Check for Dash
+	void CheckDash()
+	{
+		
+	}
+
+	//Apply Velocity Calcluations 
+	void ApplyJump()
+	{
+		finalVel = CalclulateVel (jumpHeight, rb.velocity.y, 1.0f);
+		acceleration = CalclulateAccel (finalVel, rb.velocity.y, 1.0f);
+		jumpThrust = CalclulateJumpVel (rb.mass, acceleration);
+
+		rb.AddForce(transform.right * jumpThrust, ForceMode2D.Impulse);
+	}
+
+	// Calculate velocity
+	float CalclulateVel(float distance, float initialVel,float time)
+	{
+		return(distance / time) - initialVel / 2;
+	}
+
+	// Calclulate Acceleration
+	float CalclulateAccel(float finalVel, float initialVel, float time)
+	{
+		return(finalVel - initialVel) / time;
+	}
+
+	// Calclulate Jump Velocity 
+	float CalclulateJumpVel(float mass, float acceleration)
+	{
+		return mass * acceleration;
 	}
 }

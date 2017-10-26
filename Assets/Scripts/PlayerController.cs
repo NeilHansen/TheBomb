954cfbc49602e236572f5a691b06e0f23773f6d7
NeilHansen <neil.hansen17@gmail.com>
2017-10-26 12:16:28 -0400

    added rewird, and mapped player 1, started messing with movement all funky right now

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class PlayerController : MonoBehaviour {
 
-
+	public int playerId = 0;
 	private Rigidbody2D rb;
+	private Player player;
+	private Vector3 moveVector;
+	private bool dash;
+	private bool jump;
 	public float jumpHeight;
 	public float dashThrust;
+	public float moveSpeed;
 
 	private float finalVel;
 	private float acceleration;
@@ -19,6 +25,7 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = this.GetComponent<Rigidbody2D> ();
+		player = ReInput.players.GetPlayer(playerId);
 	}
 
 	//Check if your on the ground
@@ -33,18 +40,32 @@ public class PlayerController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		GetInput ();
 		CheckJump ();
-		CheckDash ();
+		CheckMovement ();
+	}
 
+	void FixedUpdate()
+	{
+		
+		CheckDash ();
+	}
+
+	void GetInput()
+	{
+		moveVector.x = player.GetAxis("MoveX"); // get input by name or action id
+		jump = player.GetButton("Jump");
+		dash = player.GetButton ("Dash");
 	}
 
 	//Check For Jump
 	void CheckJump()
 	{
-		if (Input.GetKeyDown("space")) 
+		if (player.GetButtonDown("Jump")) 
 		{
 			if (isOnGround)
 			{
+				//rb.velocity = new Vector3 (rb.velocity.x, 0);
 				ApplyJump ();
 				canDoubleJump = true;
 				isOnGround = false;
@@ -54,16 +75,30 @@ public class PlayerController : MonoBehaviour {
 				if (canDoubleJump) 
 				{
 					canDoubleJump = false;
+					//rb.velocity = new Vector2(rb.velocity.x	, 0);
 					ApplyJump ();
 				}
 			}
 		}
 	}
+		
 
 	//Check for Dash
 	void CheckDash()
 	{
 		
+	}
+
+	void CheckMovement()
+	{
+		if(moveVector.x != 0.0f || moveVector.y != 0.0f) {
+			//cc.Move(moveVector * moveSpeed * Time.deltaTime);
+			rb.MovePosition(this.transform.position + moveVector * moveSpeed * Time.deltaTime);
+		}     
+//		if (moveVector.x != 0.0f) {
+//			//cc.Move(moveVector * moveSpeed * Time.deltaTime);
+//			rb.AddForce (this.transform.forward + moveVector * moveSpeed * Time.deltaTime, ForceMode2D.Impulse);
+//		}
 	}
 
 	//Apply Velocity Calcluations 
